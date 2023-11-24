8e1c2d4cfb3a4ca97edde64050ba2a849e2f5902
Niko-krasulin <krasulin.niko.test@gmail.com>
2023-11-24 13:52:59 +0800

    add lesson 3 index01.js

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,1 +1,8 @@
-// code
+const colors = ["синий", "черный", "зеленый", "красный", "желтый", "зеленый"];
+
+function createColorString() {
+    let a = colors.filter(item => item === 'черный' || item === 'красный' || item === 'желтый' )
+    return a.join('-')
+}
+
+console.log(createColorString());
